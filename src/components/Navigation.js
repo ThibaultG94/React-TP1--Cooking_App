2ccb2f242b659ca9a31732cf9b7e2f3592226566
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-02 19:56:09 +0100

    home css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Navigation = () => {
-    return (
-        <div>
-            <input type="text" placeholder="beef" />
-        </div>
-    );
+	return (
+		<div className="search">
+			<input
+				type="text"
+				placeholder="Tapez le nom d'un aliment (en anglais)"
+			/>
+		</div>
+	);
 };
 
 export default Navigation;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Title = () => {
-    return (
-        <div>
-            <h1>React Cook</h1>
-        </div>
-    );
+	return <h1>React Cook</h1>;
 };
 
 export default Title;
