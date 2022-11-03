30fd0e134386433f6a04c82ce5b7a25dfdf11262
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-03 21:36:09 +0100

    simplification

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from './components/Card';
 
 const App = () => {
-  return (
- <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
- </BrowserRouter>
-  );
+	const [data, setData] = useState([]);
+	const [textInput, setTextInput] = useState('');
+
+	useEffect(() => {
+		axios
+			.get('https://www.themealdb.com/api/json/v1/1/search.php?s')
+			.then((res) => setData(res.data.meals));
+	}, []);
+
+	return (
+		<div className="app">
+			<h1>React Cook</h1>
+			<input
+				type="text"
+				placeholder="Tapez le nom d'un aliment (en anglais)"
+				onChange={(e) => setTextInput(e.target.value)}
+			/>
+			<ul className="meals">
+				{data
+					.filter((meal) =>
+						meal.strMeal
+							.toLowerCase()
+							.includes(textInput.toLowerCase())
+					)
+					.map((meal, index) => (
+						<Card key="index" meal={meal} />
+					))}
+			</ul>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Title.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Title = () => {
-	return <h1>React Cook</h1>;
-};
-
-export default Title;
